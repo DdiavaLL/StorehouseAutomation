b229c95a4f4277b06a6c7bf4d5d74897ae57eb9d
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-02-23 18:57:01 +0300

    Updated part of the visualization, made a floor grid.

--- modeling.py
+++ modeling.py
@@ -1,15 +1,33 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 
 fig = plt.figure()
+ax = plt.subplot()
 
-x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Variant 1
+x_length = np.linspace(0, 10)
+# y1_width = 0
+# y2_width = 10
+# ax.fill_between(x_length, y1_width, y2_width, color='black')
 
+# Variant 2
+ax.set_facecolor("black")
+y1_width = 0
+y2_width = 10
+ax.fill_between(x_length, y1_width, y2_width, color='#F0E6E6')
+
+# Defining the coordinate system
+x = np.linspace(0, 10)
+y = np.linspace(0, 10)
+
+# Setting the grid parameters
 plt.title('Storehouse', color='white')
 plt.xlabel('Length', color='white')
 plt.ylabel('Width', color='white')
 
 plt.plot(x, y)
-plt.grid(color='black', axis='both')
+plt.grid(color='black', linewidth=2, axis='both')
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
 plt.show()
