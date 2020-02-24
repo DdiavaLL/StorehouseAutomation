dec6fd4aed2ee9c27df9911f9f04f8b8a69a2812
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-02-24 20:14:23 +0300

    Added functions for moving the agent.

--- modeling.py
+++ modeling.py
@@ -5,9 +5,16 @@ import numpy as np
 fig = plt.figure()
 ax = plt.subplot()
 
+# Coordinates of the upper-left points occupied by the box.
+box_coord = []
+
 # Defining the coordinate system
-x = [i for i in range(0, 5)]
-y = [i for i in range(0, 5)]
+x = [i for i in range(0, 25)]
+y = [i for i in range(0, 25)]
+
+# Coordinates of the agent.
+ag_coord = [(len(x)-1)/2+0.5, -0.5]
+ag_box = True
 
 # Variant 1
 x_length = np.linspace(0, len(x))
@@ -21,12 +28,12 @@ y1_width = 0
 y2_width = len(y)
 ax.fill_between(x_length, y1_width, y2_width, color='#F0E6E6')
 
-# Setting the grid parameters
+# Setting the grid parameters.
 plt.title('Storehouse', color='white')
 plt.xlabel('Length', color='white')
 plt.ylabel('Width', color='white')
 
-# Drawing the walls of the storehouse
+# Drawing the walls of the storehouse.
 w1X = [x[0]-1, x[0]]
 w1Y = [y[len(y)-1]+2, y[len(y)-1]+1]
 
@@ -39,14 +46,33 @@ w3Y = [y[len(y)-1]+1, y[len(y)-1]+2]
 w4X = [x[len(x)-1]+1, x[len(x)-1]+2]
 w4Y = [y[0],y[0]-1]
 
-# Drawing graphs
+# Drawing graphs.
 plt.plot(w1X, w1Y, color='red')
 plt.plot(w2X, w2Y, color='red')
 plt.plot(w3X, w3Y, color='red')
 plt.plot(w4X, w4Y, color='red')
-plt.scatter((len(x)-1)/2+0.5, -0.5, color='green')      # Agent
+plt.scatter(ag_coord[0], ag_coord[1], color='blue')      # Agent
 
 plt.grid(color='black', linewidth=2, axis='both')
 ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
 ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
 plt.show()
+
+
+# Fills in the grid cell occupied by the box.
+def box_in_position(x1, y1, y2):
+    ax.fill_between(x1, y1, y2, color='green')
+    box_coord.append((x1, y1))
+
+# Fills in the area that the agent left.
+def ag_fill():
+    if ag_coord[1] < 0:
+        ax.fill_between(0, 0, -3, color='black')
+    elif ag_box:
+        ax.fill_between(ag_coord[0]-0.5,ag_coord[1]+0.5,ag_coord[1]-0.5, color='#F0E6E6')
+    else:
+        box_in_position(ag_coord[0]-0.5,ag_coord[1]+0.5,ag_coord[1]-0.5)
+
+# Agent moves.
+def ag_moves():
+    plt.scatter(ag_coord[0], ag_coord[1], color='blue')
